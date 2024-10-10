ab3bd84d44cf03455c1d38d9de3b55326b3a48c0
Calebe Ribeiro <93345656+Calebeans@users.noreply.github.com>
2024-10-10 17:10:31 -0300

    Basic R study

---
+++ Aula-1-R.R
@@ -0,0 +1,47 @@
+#declarando uma variável
+x <- 10
+y <- 20
+a <- x+y
+
+x <- x+y 
+y <- x*y
+
+remove(x,a)
+
+c <- 20.3
+
+a + c
+
+b <- "Calebe"
+
+d <- 'Calebe'
+
+a + b
+
+variavel1 <- "Texto"
+
+print(variavel1)
+
+#comparação de variáveis
+is.numeric(variavel1)
+is.character(variavel1)
+is.logical(variavel1)
+
+variavel3 <- 92.54
+
+#Obter parte inteira de uma variável decimal
+as.integer(variavel3)
+variavel3
+
+variavel2 <- TRUE
+variavel2 
+print(variavel2)
+
+#cancatenação
+d <- paste("casa ", "velha", variavel3)
+print(d)
+
+paste("Oi", "Olá", "Bom", "bem", sep = "|")
+
+
+
